4e1b92fe1caba66c37dc904a6e298d00e88f33ec
GNSM <79547366+gnsmam@users.noreply.github.com>
2025-03-02 08:45:53 +0100

    Add files via upload

---
+++ install_config.py
@@ -0,0 +1,20 @@
+import os
+package_string = ""
+packages_file = open("packages.txt", "r")
+for package in packages_file:
+    package = package.rstrip("\n")
+    package = package + " "
+    package_string = package_string + package
+
+print(package_string)
+#os.system("sudo pacman -Syu")
+#os.system("sudo pacman -S " + package_string)
+
+#foot config install
+os.system("cp -rf foot.ini $HOME/.config/foot")
+
+#fuzzel config install
+os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel")
+
+#waybar config install 
+os.system("cp -rf waybar $HOME/.config")
