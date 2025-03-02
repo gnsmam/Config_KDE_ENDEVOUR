2c6132463fd1e6a74d19a89ae5d5531d4083bfb3
GNSM <79547366+gnsmam@users.noreply.github.com>
2025-03-02 09:37:59 +0100

    Added waybar to autostart, remove stock kde panels

--- install_config.py
+++ install_config.py
@@ -6,11 +6,14 @@ for package in packages_file:
     package = package + " "
     package_string = package_string + package
 
-#print(package_string)
+os.system("echo ###UPDATING PACKAGES###")
+os.system
 os.system("sudo pacman -Syu")
 os.system("sudo pacman -S " + package_string)
 
+
 #foot config install
+os.system("echo ###INSTALLING FOOT CONFIG###")
 if os.path.exists("$HOME/.config/foot/"):
     os.system("cp -rf foot.ini $HOME/.config/foot/")
 else:
@@ -19,6 +22,7 @@ else:
 
 
 #fuzzel config install
+os.system("echo ###INSTALLING FUZZEL CONFIG###")
 if os.path.exists("$HOME/.config/fuzzel/"):
     os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel/")
 else:
@@ -26,4 +30,18 @@ else:
     os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel/")
 
 #waybar config install 
+os.system("echo ###INSTALLING WAYBAR CONFIG###")
 os.system("cp -rf waybar $HOME/.config/")
+
+#add waybar to autostart
+os.system("echo ###ADDING WAYBAR TO AUTOSTART###")
+if os.path.exists("$HOME/.config/autostart/waybar.desktop"):
+    os.system("cp -rf waybar.dekstop $HOME/.config/autostart/waybar.desktop")
+else:
+    os.system("cp -rf waybar.dekstop $HOME/.config/autostart/")
+
+#remove kde panels
+os.system("echo ###REMOVING KDE PANELS###")
+os.system("rm $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc")
+
+os.system("echo ###Please go into the KDE settings panel and import keybindings from 'keybindings.kksrc' file manually###")
