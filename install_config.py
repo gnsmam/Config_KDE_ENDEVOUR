ec7f89cb992708ef2c0021d3778246f23adab283
GNSM <79547366+gnsmam@users.noreply.github.com>
2025-03-02 09:12:48 +0100

    Update install_config.py

--- install_config.py
+++ install_config.py
@@ -11,19 +11,19 @@ os.system("sudo pacman -Syu")
 os.system("sudo pacman -S " + package_string)
 
 #foot config install
-if os.path.exists("$HOME/.config/foot"):
-    os.system("cp -rf foot.ini $HOME/.config/foot")
+if os.path.exists("$HOME/.config/foot/"):
+    os.system("cp -rf foot.ini $HOME/.config/foot/")
 else:
-    os.system("mkdir $HOME/.config/foot")
+    os.system("mkdir $HOME/.config/foot/")
     os.system("cp -rf fuzzel.ini $HOME/.config/foot/")
 
 
 #fuzzel config install
 if os.path.exists("$HOME/.config/fuzzel/"):
-    os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel")
+    os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel/")
 else:
-    os.system("mkdir $HOME/.config/fuzzel")
+    os.system("mkdir $HOME/.config/fuzzel/")
     os.system("cp -rf fuzzel.ini $HOME/.config/fuzzel/")
 
 #waybar config install 
-os.system("cp -rf waybar $HOME/.config")
+os.system("cp -rf waybar $HOME/.config/")
